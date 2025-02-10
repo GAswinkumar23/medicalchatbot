78b755dc514079500010954b018111c9603d3bd1
GAswinkumar23 <g.aswin1001@gmail.com>
2025-02-10 22:01:32 +0530

    folder structured is created

---
+++ template.py
@@ -0,0 +1,35 @@
+import os
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s: %(message)s'
+)
+
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "setup.py",
+    "app.py",
+    "research/trials.ipynb",
+    "test.py"
+]
+
+for filepath in list_of_files:
+    # Ensure the filepath is correctly interpreted
+    filedir, filename = os.path.split(filepath)
+
+    # Create directories if they don't exist
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+
+    # Create the file if it doesn't exist or is empty
+    if not os.path.exists(filepath) or os.path.getsize(filepath) == 0:
+        with open(filepath, "w") as f:
+            pass
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} already exists")
